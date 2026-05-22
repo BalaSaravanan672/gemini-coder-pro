21569d05722891ccf2fcb921bdd799f5ae96f2f7
Bala Saravanan <142132698+BalaSaravanan672@users.noreply.github.com>
2026-05-22 12:21:44 +0530

    feat: add command registry infrastructure

---
+++ src/commands/index.ts
@@ -0,0 +1,5 @@
+import { CommandRegistry } from '../core/commands.js';
+
+export function registerAllCommands() {
+  // We will add commands here in subsequent tasks
+}
---
+++ src/core/commands.ts
@@ -0,0 +1,21 @@
+export interface CommandHandler {
+  name: string;
+  description: string;
+  execute(orchestrator: any, args: string[]): Promise<void>;
+}
+
+export class CommandRegistry {
+  private static commands = new Map<string, CommandHandler>();
+
+  static register(handler: CommandHandler) {
+    this.commands.set(handler.name, handler);
+  }
+
+  static get(name: string): CommandHandler | undefined {
+    return this.commands.get(name);
+  }
+
+  static getAll(): CommandHandler[] {
+    return Array.from(this.commands.values());
+  }
+}
