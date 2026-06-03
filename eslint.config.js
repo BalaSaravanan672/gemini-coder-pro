5c34b2ecf63061294575b33db4427a4f01684bac
Bala Saravanan <142132698+BalaSaravanan672@users.noreply.github.com>
2026-06-03 10:43:45 +0530

    chore: add ESLint, Prettier, and Vitest configurations

---
+++ eslint.config.js
@@ -0,0 +1,28 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import prettierConfig from "eslint-config-prettier";
+
+export default [
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      ...prettierConfig.rules,
+    },
+  },
+  {
+    ignores: ["dist/**", "node_modules/**", "temp-non-git/**"],
+  },
+];
